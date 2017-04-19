c13b7d4bad0ef6e28015aba19338fd56c2595c17
Eric Nielsen <ericnielsen33@gmail.com>
2017-04-18 22:43:45 -0600

    preped files for deployment

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ var express = require('express');
 // Create app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+})
+
 app.use(express.static('public'));
 
 
-// Listen on port 3000
+// Listen on port , which depends on the environment
 
-app.listen(3000, function (){
-    console.log('Server is running on port 3000');
+app.listen(PORT, function (){
+    console.log('Server is running on port ' + PORT);
 });
