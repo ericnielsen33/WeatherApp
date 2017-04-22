17cb0932321ac124dcc36925c5b985ebd767c4bc
Eric Nielsen <ericnielsen33@gmail.com>
2017-04-22 12:58:16 -0600

    styled the about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 const About = (props) => {
      return(
             <div>
-                <h2>About</h2>
-                <p>You can use this application to get current weather conditions in the US.</p>
-                <p></p>
-                <p>Enter a city in the search bar to get the current temperature.</p>
+                <h1 classNam="text-center">About</h1>
+                <dl>
+                    <dt>Function</dt>
+                    <dd>You can use this application to get current weather conditions in the US.</dd>
+                    <dt>How to use it</dt>
+                    <dd>Enter a city in the search bar to get the current temperature</dd>
+                    <dt>Check it out on Github</dt>
+                    <dd><a href="https://github.com/ericnielsen33/WeatherApp" rel="noopener noreferrer" target="_blank">The WeatherApp Github Repo</a></dd>
+                </dl>
             </div>
             );
 }
