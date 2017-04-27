20d50c807a3257078516769f108ab9768cc76e2a
Eric Nielsen <ericnielsen33@gmail.com>
2017-04-27 09:21:37 -0600

    updated styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = (props) => {
      return(
             <div>
-                <h1 className="text-center">About</h1>
+                <h1 className="text-center page-title">About</h1>
                 <dl>
                     <dt>Function</dt>
                     <dd>You can use this application to get current weather conditions in the US.</dd>
